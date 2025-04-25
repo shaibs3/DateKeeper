f5a9e355a19e3b6df9c439c578516cb3c1fb1ecf
sbenshalom <sbenshalom@microsoft.com>
2025-04-25 17:48:06 +0300

    fix volnurabilites

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,41 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
-import GoogleProvider from 'next-auth/providers/google';
-import type { NextAuthOptions } from 'next-auth';
+import Google from 'next-auth/providers/google';
+import NextAuth from 'next-auth';
 
-export const authOptions: NextAuthOptions = {
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
+    Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
-    async session({ session, user }) {
+    session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     },
   },
-}; 
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signin',
+    error: '/auth/signin',
+  },
+}); 
